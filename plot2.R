84aaab3073d7ccf39e376eb3da2f09ab6095565f
Raymond Chua <ray.r.chua@gmail.com>
2014-09-06 14:47:20 +0800

    add save png info to plot2.R

--- plot2.R
+++ plot2.R
@@ -4,4 +4,6 @@ dataset[,1] <- as.Date(dataset$Date, "%d/%m/%Y")
 temp <- subset(dataset, as.Date(Date) >= '2007-02-01' & as.Date(Date) <= '2007-02-02') #take only the rows needed
 temp[,3] <- as.character(temp[,3]) #convert from integer to character
 temp[,3] <- as.numeric(temp[,3]) #convert to decimals
+png(filename="plot2.png", width=480, height=480)
 plot(temp$datetime, temp$Global_active_power, type="l", xlab = "",ylab = "Global Active Power(kilowatts)")
+dev.off()
